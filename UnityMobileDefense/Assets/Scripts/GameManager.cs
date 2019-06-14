e86d25024b0baa0e29335917f70f3de9f0e23d32
qpsl72 <51826086+qpsl72@users.noreply.github.com>
2019-06-14 22:44:23 +0900

    게임 시작 로그

--- UnityMobileDefense/Assets/Scripts/GameManager.cs
+++ UnityMobileDefense/Assets/Scripts/GameManager.cs
@@ -119,6 +119,7 @@ public class GameManager : MonoBehaviour {
         nextRound();
         select(1);
         lifeText.text = life.ToString();
+        Debug.Log("게임이 시작됨");
 	}
 	
 	void Update () {
